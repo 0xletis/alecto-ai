f6cd286834f37d0c707afcb594ff9ac420a8700a
letis <letiskate@gmail.com>
2026-08-19 01:05:28 +0200

    refactor: remove unused core conversation orchestrator types

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,6 @@
 export * from "./action-intake.js";
 export * from "./checkins.js";
 export * from "./conversation-control.js";
-export * from "./conversation-orchestrator.js";
 export * from "./custom-goals.js";
 export * from "./daily-review.js";
 export * from "./daily-priority.js";
